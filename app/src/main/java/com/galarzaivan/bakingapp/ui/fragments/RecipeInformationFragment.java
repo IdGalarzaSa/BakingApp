e20077bafc7c9f32893e9f48518e67c2a94c6995
Ivan Galarza <id.galarza.sa@gmail.com>
2020-06-06 22:04:40 -0400

    exo player

--- app/src/main/java/com/galarzaivan/bakingapp/ui/fragments/RecipeInformationFragment.java
+++ app/src/main/java/com/galarzaivan/bakingapp/ui/fragments/RecipeInformationFragment.java
@@ -1,6 +1,8 @@
 package com.galarzaivan.bakingapp.ui.fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,11 +16,18 @@ import android.widget.TextView;
 
 import com.galarzaivan.bakingapp.R;
 import com.galarzaivan.bakingapp.classes.AppConstants;
-import com.galarzaivan.bakingapp.models.Ingredient;
 import com.galarzaivan.bakingapp.models.Step;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.google.gson.Gson;
 
-import java.util.Arrays;
+import java.net.URL;
 
 public class RecipeInformationFragment extends Fragment {
 
@@ -28,6 +37,13 @@ public class RecipeInformationFragment extends Fragment {
     private Context mContext;
     // Views
     private TextView mStepInstructions;
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private String mURL;
+
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
 
     public RecipeInformationFragment() {
         // Required empty public constructor
@@ -43,17 +59,15 @@ public class RecipeInformationFragment extends Fragment {
 
         if (savedInstanceState != null && savedInstanceState.containsKey(STEP)) {
             String previousData = savedInstanceState.getString(STEP);
-            Log.e("prueba", "onCreateView------------->" + previousData);
             mStep = new Gson().fromJson(previousData, Step.class);
-            loadMedia();
             loadData();
+            loadMedia();
         } else {
-            Log.e("prueba", "NO------------->");
             if (getArguments() != null && getArguments().containsKey(AppConstants.STEP_INFORMATION)) {
                 String data = getArguments().getString(AppConstants.STEP_INFORMATION);
                 mStep = new Gson().fromJson(data, Step.class);
-                loadMedia();
                 loadData();
+                loadMedia();
             }
         }
 
@@ -62,14 +76,52 @@ public class RecipeInformationFragment extends Fragment {
 
     private void initViews(View view) {
         mStepInstructions = (TextView) view.findViewById(R.id.tv_step_instructions);
+        playerView = view.findViewById(R.id.video_view);
     }
 
     private void loadMedia() {
+        if (mStep.getVideoURL() != null && !mStep.getVideoURL().equals("")) {
+            playerView.setVisibility(View.VISIBLE);
+            mURL = mStep.getVideoURL();
+            initializePlayer();
+        } else if (mStep.getThumbnailURL() != null && !mStep.getThumbnailURL().equals("")) {
+            playerView.setVisibility(View.VISIBLE);
+            mURL = mStep.getThumbnailURL();
+            initializePlayer();
+        } else {
+            playerView.setVisibility(View.GONE);
+        }
+    }
 
+    private void initializePlayer() {
+        player = ExoPlayerFactory.newSimpleInstance(mContext);
+        playerView.setPlayer(player);
+        Uri uri = Uri.parse(mURL);
+        MediaSource mediaSource = buildMediaSource(uri);
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        player.prepare(mediaSource, false, false);
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(mContext, getString(R.string.baking_app_name));
+        return new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(uri);
     }
 
     private void loadData() {
         mStepInstructions.setText(mStep.getDescription());
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playWhenReady = player.getPlayWhenReady();
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            player.release();
+            player = null;
+        }
     }
 
     @Override
@@ -77,5 +129,48 @@ public class RecipeInformationFragment extends Fragment {
         super.onSaveInstanceState(outState);
         String data = new Gson().toJson(mStep);
         outState.putString(STEP, data);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT >= 24 && mURL != null) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT < 24 || player == null) && mURL != null) {
+            initializePlayer();
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT < 24) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT >= 24) {
+            releasePlayer();
+        }
     }
 }
